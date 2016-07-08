64ef3792fc0fa9f7f0f2665c249bdc796245bfaf
Henrik Adam Hasell <henrikhasell@gmail.com>
2016-07-08 13:42:22 +0100

    Simplified the art style.

--- main.cpp
+++ main.cpp
@@ -78,7 +78,6 @@ const char TILEMAP_DATA[20 * 20] = {
 
 Game::World world(20, 20, TILEMAP_DATA);
 
-bool smooth_path = true;
 std::vector<Navigation::Vector> path;
 Navigation::Vector start(495.0f, 15.0f), finish(495.0f, 585.0f);
 
@@ -93,9 +92,6 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 		switch(key)
 		{
-			case GLFW_KEY_SPACE:
-				smooth_path = !smooth_path;
-				break;
 			case GLFW_KEY_S:
 				world.soldierList.emplace_back(start, path);
 				break;
@@ -123,41 +119,41 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
-{
-	if(action == GLFW_PRESS)
-	{
-		double xpos;
-		double ypos;
-
-		glfwGetCursorPos(window, &xpos, &ypos);
-
-		switch(button)
-		{
-			case GLFW_MOUSE_BUTTON_LEFT:
-				start.x = xpos;
-				start.y = ypos;
-				break;
-			case GLFW_MOUSE_BUTTON_RIGHT:
-				finish.x = xpos;
-				finish.y = ypos;
-				break;
-		}
-	}
-}
-
-void cursor_pos_callback(GLFWwindow *window, double x, double y)
-{
-	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == true)
-	{
-		mouse_button_callback(window, GLFW_MOUSE_BUTTON_LEFT, GLFW_PRESS, 0);
-	}
-
-	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == true)
-	{
-		mouse_button_callback(window, GLFW_MOUSE_BUTTON_RIGHT, GLFW_PRESS, 0);
-	}
-}
+// void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+// {
+// 	if(action == GLFW_PRESS)
+// 	{
+// 		double xpos;
+// 		double ypos;
+// 
+// 		glfwGetCursorPos(window, &xpos, &ypos);
+// 
+// 		switch(button)
+// 		{
+// 			case GLFW_MOUSE_BUTTON_LEFT:
+// 				start.x = xpos;
+// 				start.y = ypos;
+// 				break;
+// 			case GLFW_MOUSE_BUTTON_RIGHT:
+// 				finish.x = xpos;
+// 				finish.y = ypos;
+// 				break;
+// 		}
+// 	}
+// }
+// 
+// void cursor_pos_callback(GLFWwindow *window, double x, double y)
+// {
+// 	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == true)
+// 	{
+// 		mouse_button_callback(window, GLFW_MOUSE_BUTTON_LEFT, GLFW_PRESS, 0);
+// 	}
+// 
+// 	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == true)
+// 	{
+// 		mouse_button_callback(window, GLFW_MOUSE_BUTTON_RIGHT, GLFW_PRESS, 0);
+// 	}
+// }
 
 int main(int argc, char *argv[])
 {
@@ -171,8 +167,8 @@ int main(int argc, char *argv[])
 		{
 			// Initialise GLFW:
 			glfwSetKeyCallback(window, key_callback);
-			glfwSetMouseButtonCallback(window, mouse_button_callback);
-			glfwSetCursorPosCallback(window, cursor_pos_callback);
+			// glfwSetMouseButtonCallback(window, mouse_button_callback);
+			// glfwSetCursorPosCallback(window, cursor_pos_callback);
 
 			// Create OpenGL context:
 			glfwMakeContextCurrent(window);
@@ -219,31 +215,49 @@ int main(int argc, char *argv[])
 							Tile *tile = world.nodeMap.TileMap::GetTile(x, y);
 
 							glPushMatrix();
+								glTranslatef(x, y, 0.0f);
+
 								Tile::Type type = tile->GetType();
 
 								switch(type)
 								{
-									case Tile::Type::ROAD:
-										glColor3f(0.5f, 0.5f, 0.5f); break;
-									case Tile::Type::WALL:
-										glColor3f(0.1f, 0.1f, 0.1f); break;
-									case Tile::Type::TURRET:
-										glColor3f(1.0f, 0.0f,0.0f); break;
+								case Tile::Type::ROAD:
+									glColor3f(0.5f, 0.5f, 0.5f);
+			
+									glBegin(GL_TRIANGLE_STRIP);
+										glVertex2f(0.0f, 0.0f);
+										glVertex2f(1.0f, 0.0f);
+										glVertex2f(0.0f, 1.0f);
+										glVertex2f(1.0f, 1.0f);
+									glEnd();
+
+									glColor3f(0.4f, 0.4f, 0.4f);
+
+									glBegin(GL_TRIANGLE_STRIP);
+										glVertex2f(0.5f, 0.0f);
+										glVertex2f(1.0f, 0.5f);
+										glVertex2f(0.0f, 0.5f);
+										glVertex2f(0.5f, 1.0f);
+									glEnd();
+									break;
+
+								case Tile::Type::WALL:
+									glColor3f(0.05f, 0.05f, 0.05f);
+									glBegin(GL_TRIANGLE_STRIP);
+										glVertex2f(0.0f, 0.0f);
+										glVertex2f(1.0f, 0.0f);
+										glVertex2f(0.0f, 1.0f);
+										glVertex2f(1.0f, 1.0f);
+									glEnd();
+									break;
 								}
-								glTranslatef(x, y, 0.0f);
-								glBegin(GL_TRIANGLE_STRIP);
-									glVertex2f(0.0f, 0.0f);
-									glVertex2f(1.0f, 0.0f);
-									glVertex2f(0.0f, 1.0f);
-									glVertex2f(1.0f, 1.0f);
-								glEnd();
-								glColor3f(0.0f, 0.0f, 0.0f);
-								glBegin(GL_LINE_LOOP);
-									glVertex2f(0.0f, 0.0f);
-									glVertex2f(1.0f, 0.0f);
-									glVertex2f(1.0f, 1.0f);
-									glVertex2f(0.0f, 1.0f);
-								glEnd();
+								// glColor3f(0.0f, 0.0f, 0.0f);
+								// glBegin(GL_LINE_LOOP);
+								// 	glVertex2f(0.0f, 0.0f);
+								// 	glVertex2f(1.0f, 0.0f);
+								// 	glVertex2f(1.0f, 1.0f);
+								// 	glVertex2f(0.0f, 1.0f);
+								// glEnd();
 							glPopMatrix();
 						}
 					}
@@ -253,60 +267,7 @@ int main(int argc, char *argv[])
 				// Calculate a path between the
 				// start and finish vectors if
 				// possible:
-				if(world.nodeMap.CalculatePath(start, finish, path) == true)
-				{
-					if(smooth_path)
-					{
-
-						std::vector<Navigation::Vector> minimal_path;
-
-						Navigation::Vector previousViewpoint = path[0], currentViewpoint = path[0];
-
-						minimal_path.push_back(start);
-
-						for(Navigation::Vector selectedNode : path)
-						{
-							if(world.nodeMap.Visible(currentViewpoint, selectedNode) == false)
-							{
-								minimal_path.push_back(previousViewpoint);
-								currentViewpoint = previousViewpoint;
-							}
-
-							previousViewpoint = selectedNode;
-						}
-
-						minimal_path.push_back(finish);
-
-						path = minimal_path;
-					}
-
-					glColor3f(1.0f, 0.0f, 0.0f);
-					// Draw the path (line):
-					glBegin(GL_LINE_STRIP);
-					for(Navigation::Vector &i : path)
-					{
-						glVertex2f(i.x, i.y);
-					}
-					glEnd();
-
-					glColor3f(0.0f, 0.0f, 1.0f);
-					// Draw the path (vertices):
-					glBegin(GL_POINTS);
-					for(Navigation::Vector &i : path)
-					{
-						glVertex2f(i.x, i.y);
-					}
-					glEnd();
-				}
-
-				// Draw the start and finish positions of the path.
-				glColor3f(0.0f, 1.0f, 0.0f);
-				glBegin(GL_POINTS);
-					if(start.x || start.y)
-						glVertex2f(start.x, start.y);
-					if(finish.x || finish.y)
-						glVertex2f(finish.x, finish.y);
-				glEnd();
+				world.nodeMap.CalculatePath(start, finish, path);
 
 				for(Game::Soldier &soldier : world.soldierList)
 				{
@@ -322,33 +283,36 @@ int main(int argc, char *argv[])
 							glEnd();
 							// Draw the soldier (outline).
 							glColor3f(0.0f, 0.0f, 0.0f);
-							glBegin(GL_LINE_LOOP);
-								for(GLfloat i = 0.0f; i < M_PI * 2; i += 0.3f)
-									glVertex2f( sinf(i), cosf(i) );
-							glEnd();
+							// glBegin(GL_LINE_LOOP);
+							// 	for(GLfloat i = 0.0f; i < M_PI * 2; i += 0.3f)
+							// 		glVertex2f( sinf(i), cosf(i) );
+							// glEnd();
 							// Draw the soldier (direction).
 							glBegin(GL_LINES);
 								glVertex2f(0.0f, 0.0f);
 								glVertex2f(0.0f, 1.0f);
 							glEnd();
 						glPopMatrix();
-						glTranslatef(-10.0f, -15.0f, 0.0f);
-						glColor3f(0.0f, 0.0f, 0.0f);
-						glScalef(20.0f, 5.0f, 1.0f);
-						glBegin(GL_TRIANGLE_STRIP);
-							glVertex2f(0.0f, 0.0f);
-							glVertex2f(1.0f, 0.0f);
-							glVertex2f(0.0f, 1.0f);
-							glVertex2f(1.0f, 1.0f);
-						glEnd();
-						glColor3f(1.0f, 0.0f, 0.0f);
-						glScalef(soldier.hitpoints / Game::Soldier::MaximumHitpoints, 1.0f, 1.0f);
-						glBegin(GL_TRIANGLE_STRIP);
-							glVertex2f(0.0f, 0.0f);
-							glVertex2f(1.0f, 0.0f);
-							glVertex2f(0.0f, 1.0f);
-							glVertex2f(1.0f, 1.0f);
-						glEnd();
+						if(soldier.hitpoints < Game::Soldier::MaximumHitpoints)
+						{
+							glTranslatef(-10.0f, -15.0f, 0.0f);
+							glColor3f(0.0f, 0.0f, 0.0f);
+							glScalef(20.0f, 5.0f, 1.0f);
+							glBegin(GL_TRIANGLE_STRIP);
+								glVertex2f(0.0f, 0.0f);
+								glVertex2f(1.0f, 0.0f);
+								glVertex2f(0.0f, 1.0f);
+								glVertex2f(1.0f, 1.0f);
+							glEnd();
+							glColor3f(1.0f, 0.0f, 0.0f);
+							glScalef(soldier.hitpoints / Game::Soldier::MaximumHitpoints, 1.0f, 1.0f);
+							glBegin(GL_TRIANGLE_STRIP);
+								glVertex2f(0.0f, 0.0f);
+								glVertex2f(1.0f, 0.0f);
+								glVertex2f(0.0f, 1.0f);
+								glVertex2f(1.0f, 1.0f);
+							glEnd();
+						}
 					glPopMatrix();
 				}
 
@@ -369,12 +333,12 @@ int main(int argc, char *argv[])
 							glVertex2f(+0.5f,+0.5f);
 						glEnd();
 						glColor3f(0.0f, 0.0f, 0.0f);
-						glBegin(GL_LINE_LOOP);
-							glVertex2f(-0.5f,-0.5f);
-							glVertex2f(+0.5f,-0.5f);
-							glVertex2f(+0.5f,+0.5f);
-							glVertex2f(-0.5f,+0.5f);
-						glEnd();
+						//glBegin(GL_LINE_LOOP);
+						//	glVertex2f(-0.5f,-0.5f);
+						//	glVertex2f(+0.5f,-0.5f);
+						//	glVertex2f(+0.5f,+0.5f);
+						//	glVertex2f(-0.5f,+0.5f);
+						//glEnd();
 						glScalef(0.5f, 0.5f, 1.0f);
 						glColor3f(1.0f, 0.0f, 0.0f);
 						glRotatef(turret.rotation * 57.2958f,
@@ -384,18 +348,18 @@ int main(int argc, char *argv[])
 							glVertex2f(0.0f,-1.0f);
 						glEnd();
 					glPopMatrix();
-					if(turret.target)
-					{
-						glColor3f(1.0f, 0.0f, 0.0f);
-						glBegin(GL_POINTS);
-							glVertex2f(
-								turret.target->position.x,
-								turret.target->position.y);
-						glEnd();
-					}
+					// if(turret.target)
+					// {
+					// 	glColor3f(1.0f, 0.0f, 0.0f);
+					// 	glBegin(GL_POINTS);
+					// 		glVertex2f(
+					// 			turret.target->position.x,
+					// 			turret.target->position.y);
+					// 	glEnd();
+					// }
 				}
 
-				glColor3f(0.0f, 0.0f, 1.0f);
+				glColor3f(0.2f, 0.0f, 0.0f);
 				glBegin(GL_POINTS);
 				for(Game::Bullet &bullet : world.bulletList)
 				{
